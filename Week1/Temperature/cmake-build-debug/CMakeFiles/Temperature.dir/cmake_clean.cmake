0cf0b67a0442984accb23b390ea25210b71320cf
Darren-ODonnell <darren.odonnell001@gmail.com>
2021-01-27 13:22:03 +0000

    Week One

---
+++ Week1/Temperature/cmake-build-debug/CMakeFiles/Temperature.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Temperature.dir/main.c.obj"
+  "Temperature.exe"
+  "Temperature.exe.manifest"
+  "Temperature.pdb"
+  "libTemperature.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Temperature.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
